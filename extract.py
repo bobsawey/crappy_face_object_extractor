a32a8879086ca0a86df4b0e4c4be4c5d9a79214f
bobsawey <bobsawey@gmail.com>
2018-08-01 02:01:36 -0500

    finals commits for tools-legacy code

--- extract.py
+++ extract.py
@@ -1,5 +1,44 @@
 #!/usr/bin/env python3
 
 # python3 extract.py data_in data_out
+import sys
+import cv2
+import shutil
+import logging
+from os import listdir
+from os.path import isfile, join, isdir
+import time
 
-from tools.grabcut_face_detect_auto import
+ts = int(time.time())
+
+from tools.grabcut_face_detect_auto import cutout_face
+
+logging.basicConfig(filename='extraction.log', level=logging.INFO)
+
+image_in_dir = sys.argv[1]
+image_out_dir = sys.argv[2]
+
+# TODO: needs to check if images
+images_to_process = [f for f in listdir(image_in_dir) if isfile(join(image_in_dir, f))]
+
+logging.info(f'Beginning For Loop')
+
+for image_path in images_to_process:
+
+    logging.info(f'Starting Cutout for {image_path}')
+
+    cutout, status = cutout_face(f"{image_in_dir}/{image_path}")
+
+    # if cutout comes back False, next item
+
+    if not status:
+        # move {image_in_dir}/{image_path} {image_in_dir}/error/
+        logging.info(f'Cutting not working for {image_path} ')
+        shutil.move(f'{image_in_dir}/{image_path}', f'{image_in_dir}/error/')
+        continue
+
+    logging.info('Cutout complete. ')
+    logging.info('Saving new Image. ')
+
+    cv2.imwrite(f"{image_out_dir}/{ts}.{image_path}.0.face_cut.png", cutout)
+    logging.info('Image saved.')
--- tools/detect_faces.py
+++ tools/detect_faces.py
@@ -48,7 +48,8 @@ def detect_faces(image_path):
             # probability
             text = "{:.2f}%".format(confidence * 100)
             y = startY - 10 if startY - 10 > 10 else startY + 10
-            #cv2.rectangle(image, (startX, startY), (endX, endY),
+            #cv2.rectangle(ima_rect, rectangle=faces.get_rects(image_path)
+            net = cge, (startX, startY), (endX, endY),
                 #(0, 0, 255), 1)
             padding = 30
             crop = image[(startY-padding):endY+padding,startX-padding:endX+padding]
@@ -69,17 +70,14 @@ def detect_faces(image_path):
 
 def get_rects(image_path):
 
-    args_prototxt = 'models/detect_faces.deploy.prototxt.txt'
-    args_model = 'models/detect_faces.res10_300x300_ssd_iter_140000.caffemodel'
+    args_prototxt = './tools/models/detect_faces.deploy.prototxt.txt'
+    args_model = './tools/models/detect_faces.res10_300x300_ssd_iter_140000.caffemodel'
     args_confidence = 0.9;
-
-    # load our serialized model from disk
-    print("[INFO] loading model...")
     net = cv2.dnn.readNetFromCaffe(args_prototxt, args_model)
-
     # load the input image and construct an input blob for the image
     # by resizing to a fixed 300x300 pixels and then normalizing it
     image = cv2.imread(image_path)
+    print(image_path)
     (h, w) = image.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
         (300, 300), (104.0, 177.0, 123.0))
@@ -106,6 +104,12 @@ def get_rects(image_path):
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
 
+            if startX > 500:
+                return rect_tuple, tup_tup, False
+
+            if startY > 500:
+                return rect_tuple, tup_tup, False
+                    
             # draw the bounding box of the face along with the associated
             # probability
             text = "{:.2f}%".format(confidence * 100)
@@ -113,9 +117,9 @@ def get_rects(image_path):
             #cv2.rectangle(image, (startX, startY), (endX, endY),
                 #(0, 0, 255), 1)
 
-            paddingY = int((startY/4)*math.e)
-            paddingX = int((startX/4)*math.e/2)
-
+            paddingY = int((startY/4)*(math.e))
+            paddingX = int((startX/4)*(math.e/2))
+            print(f'START X before DIVIDING BY 4={startX}')
             print((startY-paddingY))
             print(endY+paddingY)
             print(f"endy{endY}")
@@ -142,4 +146,4 @@ def get_rects(image_path):
     # show the output image
     #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    return rect_tuple, tup_tup
+    return rect_tuple, tup_tup, True
